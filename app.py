ae26ddadd62044d3d64f556e723b3597d9aba939
mg mg <gaurav.verma18@gmail.com>
2021-03-21 03:29:23 +0530

    lint success

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)             # create an app instance
 
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
-    return "Hello World!".X         # which returns "hello world"
+    return "Hello World!"         # which returns "hello world"
 if __name__ == "__main__":        # on running python app.py
     app.run()                    # run the flask app
